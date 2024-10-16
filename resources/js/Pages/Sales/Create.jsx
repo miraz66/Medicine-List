f4f1731b1db7a3770931789668d089c1fe6a8e82
miraz66 <mirajulislam0509@gmail.com>
2024-10-16 12:27:49 +0600

    Set Search for Medicine

--- resources/js/Pages/Sales/Create.jsx
+++ resources/js/Pages/Sales/Create.jsx
@@ -7,51 +7,68 @@ import SuccessMessage from "@/Components/SuccessMessage";
 export default function SaleForm({ medicines }) {
     const { data, setData, post, processing, errors, reset, wasSuccessful } =
         useForm({
-            medicine_id: "",
-            quantity: "",
-            total_price: "",
+            saleItems: [], // Array to hold multiple sale items
         });
 
     const [selectedMedicine, setSelectedMedicine] = useState(null);
+    const [quantity, setQuantity] = useState("");
+    const [saleItems, setSaleItems] = useState([]);
 
     // Update the selected medicine when the user selects one
     const handleMedicineChange = (e) => {
         const selectedId = e.target.value;
-        setData("medicine_id", selectedId);
         const medicine = medicines.find((med) => med.id == selectedId);
         setSelectedMedicine(medicine);
     };
 
-    // Update total price whenever the medicine or quantity changes
+    // Add the selected medicine and quantity to the to-do list
+    const addMedicineToList = () => {
+        if (!selectedMedicine || !quantity) return;
+
+        const totalPrice = selectedMedicine.price * quantity;
+
+        const newItem = {
+            medicine_id: selectedMedicine.id,
+            name: selectedMedicine.name,
+            quantity,
+            total_price: totalPrice,
+        };
+
+        setSaleItems((prevItems) => [...prevItems, newItem]);
+        setQuantity(""); // Reset quantity after adding
+        setSelectedMedicine(null); // Reset selected medicine
+    };
+
+    // Remove an item from the sale list
+    const removeItem = (index) => {
+        setSaleItems((prevItems) => prevItems.filter((_, i) => i !== index));
+    };
+
+    // Update form data when sale items change
     useEffect(() => {
-        if (selectedMedicine && data.quantity) {
-            const total = data.quantity * selectedMedicine.price;
-            setData("total_price", total); // Set total price in form data
-        }
-    }, [selectedMedicine, data.quantity]); // Recalculate total price when either changes
+        setData("saleItems", saleItems); // Update form data with the sale items list
+    }, [saleItems]);
 
     const submit = (e) => {
         e.preventDefault();
-
         post(route("sales.store"), {
             onSuccess: () => reset(),
         });
+        setSaleItems([]);
     };
 
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     useEffect(() => {
         if (wasSuccessful) {
             setShowSuccessMessage(true);
-
-            // Hide success message after 5 seconds
             const timeout = setTimeout(() => {
                 setShowSuccessMessage(false);
             }, 3000);
-
-            // Cleanup the timeout when the component unmounts or when wasSuccessful changes
             return () => clearTimeout(timeout);
         }
     }, [wasSuccessful]);
+
+    // console.log(data);
 
     return (
         <div className="bg-gray-600 min-h-screen pt-40">
@@ -76,8 +93,7 @@ export default function SaleForm({ medicines }) {
                         />
                         <select
                             id="medicine"
-                            name="medicine_id"
-                            value={data.medicine_id}
+                            value={selectedMedicine?.id || ""}
                             onChange={handleMedicineChange}
                             className="mt-1 block w-full text-gray-300 bg-gray-100 dark:bg-gray-600 p-2"
                         >
@@ -85,46 +101,72 @@ export default function SaleForm({ medicines }) {
                             {medicines.map((medicine) => (
                                 <option key={medicine.id} value={medicine.id}>
                                     {medicine.name} (Stock: {medicine.stock})
+                                    (Price: {medicine.price})
                                 </option>
                             ))}
                         </select>
-                        {errors.medicine_id && (
-                            <div className="text-sm text-red-600">
-                                {errors.medicine_id}
-                            </div>
-                        )}
                     </div>
-
                     {/* Quantity Field */}
                     <div className="mt-4">
                         <InputLabel htmlFor="quantity" value="Quantity" />
                         <TextInput
                             id="quantity"
-                            name="quantity"
                             type="number"
-                            value={data.quantity}
-                            onChange={(e) =>
-                                setData("quantity", e.target.value)
-                            }
+                            value={quantity}
+                            onChange={(e) => setQuantity(e.target.value)}
                             className="mt-1 block w-full bg-gray-100 dark:bg-gray-700 p-2"
                         />
-                        {errors.quantity && (
-                            <div className="text-sm text-red-600">
-                                {errors.quantity}
+                    </div>
+                    {/* total price: */}
+                    {selectedMedicine && (
+                        <div className="mt-4">
+                            <span className="text-gray-300">
+                                Total: ${selectedMedicine?.price * quantity}
+                            </span>
+                        </div>
+                    )}
+                    {/* Add to Sale List Button */}
+                    <div className="mt-4">
+                        <button
+                            type="button"
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={addMedicineToList}
+                        >
+                            Add to Sale List
+                        </button>
+                    </div>
+                    {/* Display Sale Items (To-Do List) */}
+                    <div className="mt-4">
+                        {saleItems.length > 0 && (
+                            <div>
+                                <h2 className="text-lg text-gray-100">
+                                    Sale Items:
+                                </h2>
+                                <ul>
+                                    {saleItems.map((item, index) => (
+                                        <li
+                                            key={index}
+                                            className="flex justify-between items-center mt-2"
+                                        >
+                                            <span className="text-gray-300">
+                                                {item.name} - {item.quantity}{" "}
+                                                units - $ {item.total_price}
+                                            </span>
+                                            <button
+                                                type="button"
+                                                onClick={() =>
+                                                    removeItem(index)
+                                                }
+                                                className="text-red-500 hover:text-red-700"
+                                            >
+                                                Remove
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
                             </div>
                         )}
                     </div>
-
-                    {/* Total Price */}
-                    {selectedMedicine && data.quantity && (
-                        <div className="mt-4">
-                            <InputLabel value="Total Price" />
-                            <p className="text-gray-100">
-                                $ {data.total_price}
-                            </p>
-                        </div>
-                    )}
-
                     {/* Submit Button */}
                     <div className="flex items-center justify-between mt-4">
                         <Link
